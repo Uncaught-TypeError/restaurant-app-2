fb7953078a95721e92660c1c0a5ea21b377c8931
Nick <95492327+NickAcm21@users.noreply.github.com>
2023-08-25 00:53:48 +0630

    navbar edit

--- src/components/SecondSec/SecondSec.jsx
+++ src/components/SecondSec/SecondSec.jsx
@@ -6,13 +6,13 @@ const SecondSec = () => {
   return (
     <div className='w-full h-screen text-black'>
         <div className='flex flex-col justify-center items-center w-full h-full bg-[#f5f5f5]'>
-            <div className='max-w-[1000px] w-full grid grid-cols-2 relative'>
-                <div className='flex flex-col justify-evenly z-20'>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 relative'>
+                <div className='flex flex-col justify-evenly items-center z-20'>
                     <div className='p-6'>
-                        <img src={images.eggbene} className='transform hover:scale-105 transition-all duration-300' />
+                        <img src={images.eggbene} className='xl:h-[300px] h-[250px] transform hover:scale-105 transition-all duration-300' />
                     </div>
-                    <div className='font-Cormorant flex flex-col text-5xl'>
-                        <div className='p-2'>
+                    <div className='font-Cormorant flex flex-col md:text-5xl sm:text-4xl text-3xl'>
+                        <div className='p-2 flex flex-col sm:items-start items-center'>
                             <span className=''>Our Special for Today -</span>
                             <span className='italic'>Breakfast!</span>
                         </div>
@@ -24,18 +24,20 @@ const SecondSec = () => {
                     </div>
                 </div>
                 <div className='flex flex-col p-4 items-center z-20'>
-                    <div className='p-4'>
-                        <img src={images.burg} className='h-[450px] transform hover:scale-105 transition-all duration-300' />
+                    <div className='p-4 sm:block hidden'>
+                        <img src={images.burg} className='lg:h-[400px] md:h-[350px] h-[300px] transform hover:scale-105 transition-all duration-300' />
                     </div>
                     <div className='p-4'>
-                        <img src={images.coffee} className='transform hover:scale-105 transition-all duration-300' />
+                        <img src={images.coffee} className='xl:h-[300px] h-[250px] transform hover:scale-105 transition-all duration-300' />
                     </div>
                 </div>
-                <div className='absolute bg-slate-300 w-[400px] h-[300px] top-[12%] left-0'></div>
-                <div className='absolute bg-slate-200 w-[200px] h-[150px] top-[38%] left-[30%]'></div>
-                <div className='absolute bg-slate-300 w-[300px] h-[400px] top-[12%] right-[5%]'></div>
-                <div className='absolute bg-slate-300 w-[420px] h-[250px] bottom-0 right-[8%]'></div>
-                <div className='absolute bg-slate-200 w-[300px] h-[200px] bottom-[10%] left-0'></div>
+                <div className='sm:block hidden'>
+                    <div className='absolute bg-slate-300 xl:w-[400px] md:w-[350px] w-[300px] xl:h-[300px] h-[250px] top-[12%] xl:left-0 left-[10%]'></div>
+                    <div className='absolute xl:block hidden bg-slate-200 xl:w-[200px] xl:h-[150px] top-[38%] left-[30%]'></div>
+                    <div className='absolute bg-slate-300 xl:w-[300px] xl:h-[400px] w-[200px] h-[300px] xl:top-[12%] top-0 xl:right-[5%] right-[10%]'></div>
+                    <div className='absolute bg-slate-300 xl:w-[420px] w-[380px] h-[250px] bottom-0 right-[8%]'></div>
+                    <div className='absolute bg-slate-200 xl:w-[300px] h-[200px] w-[280px] bottom-[10%] xl:left-0 left-[5%]'></div>
+                </div>
             </div>
         </div>
     </div>
--- src/components/SideNavs/SideNavs.jsx
+++ src/components/SideNavs/SideNavs.jsx
@@ -11,23 +11,32 @@ const SideNavs = () => {
     const handleMouseLeave = () => {
         setIsHovered(false);
     };
+    const [isOn, setIsOn] = useState(false);
+
+    const OnHover = () => {
+        setIsOn(true);
+    };
+
+    const OnMouseLeave = () => {
+        setIsOn(false);
+    };
   return (
-    <div className='fixed text-black'>
+    <div className='fixed text-black z-30'>
         <div className='2xl:fixed 2xl:block hidden top-[45%] left-[200px]'>
             <span className='2xl:rotate-90 rotate-0 fixed font-Cormorant hover:text-yellow-400 text-2xl text-black tracking-wider'>
                 <a href=''>See Our Menu</a>
             </span>
         </div>
         <div className='fixed top-[90%] xl:left-[250px] md:left-[100px] left-[1rem]'>
-            <img src={isHovered ? images.egg2 : images.egg1} className='h-12' onMouseEnter={handleHover}
+            <img src={isHovered ? images.egg2 : images.egg1} className='h-12 z-50' onMouseEnter={handleHover}
             onMouseLeave={handleMouseLeave} />
         </div>
         <div className='2xl:fixed 2xl:block hidden top-[40%] right-[250px]'>
             <div className='bg-black h-[150px] w-1'></div>
         </div>
         <div className='fixed top-[90%] xl:right-[200px] md:right-[100px] right-[1rem]'>
-            <img src={isHovered ? images.msg1 : images.msg2} className='h-11' onMouseEnter={handleHover}
-            onMouseLeave={handleMouseLeave} />
+            <img src={isOn ? images.msg1 : images.msg2} className='h-11' onMouseEnter={OnHover}
+            onMouseLeave={OnMouseLeave} />
         </div>
     </div>
   )
